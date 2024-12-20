32941462a3c2ce6975eb633b17e8f58e41b79a3a
Jade <154241628+BacaranJade@users.noreply.github.com>
2024-12-21 03:44:04 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,19 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.3.0/workbox-sw.js');
 
 if (workbox) {
+    console.log('Workbox is loaded.');
+
+   
     workbox.routing.registerRoute(
         new RegExp('https://v2.jokeapi.dev/joke/Any?type=single'),
         new workbox.strategies.NetworkFirst()
     );
+
+    
+    workbox.routing.registerRoute(
+        new RegExp('https://dog.ceo/api/breeds/image/random'),
+        new workbox.strategies.NetworkFirst()
+    );
+} else {
+    console.log('Workbox failed to load.');
 }
